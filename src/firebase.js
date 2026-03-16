3d9d4686fceb691ad457ad4f95a0571cdd55cc35
kamiya0711 <sketchbooks2075@gmail.com>
2026-03-17 02:46:43 +0900

    fix firebase merge conflict

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-<<<<<<< HEAD
   apiKey: "AIzaSyC0i_PUEFPoybnsVaCGlk_J-FronkQJMIA",
   authDomain: "poker-tournament-b845f.firebaseapp.com",
   databaseURL: "https://poker-tournament-b845f-default-rtdb.firebaseio.com",
@@ -10,15 +9,6 @@ const firebaseConfig = {
   storageBucket: "poker-tournament-b845f.firebasestorage.app",
   messagingSenderId: "758629753463",
   appId: "1:758629753463:web:8485cd78a87b048a465c72"
-=======
-  apiKey:        process.env.REACT_APP_FIREBASE_API_KEY        || "AIzaSyC0i_PUEFPoybnsVaCGlk_J-FronkQJMIA",
-  authDomain:    process.env.REACT_APP_FIREBASE_PROJECT_ID     ? `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com` : "poker-tournament-b845f.firebaseapp.com",
-  databaseURL:   process.env.REACT_APP_FIREBASE_DB_URL         || "https://poker-tournament-b845f-default-rtdb.firebaseio.com",
-  projectId:     process.env.REACT_APP_FIREBASE_PROJECT_ID     || "poker-tournament-b845f",
-  storageBucket: process.env.REACT_APP_FIREBASE_PROJECT_ID     ? `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebasestorage.app` : "poker-tournament-b845f.firebasestorage.app",
-  messagingSenderId: "758629753463",
-  appId:         process.env.REACT_APP_FIREBASE_APP_ID         || "1:758629753463:web:8485cd78a87b048a465c72"
->>>>>>> dev
 };
 
 const app = initializeApp(firebaseConfig);
