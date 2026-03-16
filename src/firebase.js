c01aece6c0ff6a732efa854586ffe490b1e0ad3e
kamiya0711 <sketchbooks2075@gmail.com>
2026-03-17 02:10:07 +0900

    use env vars for firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByJ1hEUkISup2Vi23MVp1Zn0w3ttadnYA",
-  authDomain: "poker-tournament-dev.firebaseapp.com",
-  databaseURL: "https://poker-tournament-dev-default-rtdb.firebaseio.com",
-  projectId: "poker-tournament-dev",
-  storageBucket: "poker-tournament-dev.firebasestorage.app",
-  messagingSenderId: "678998245671",
-  appId: "1:678998245671:web:0e6eada42759ec1888e902"
+  apiKey:        process.env.REACT_APP_FIREBASE_API_KEY        || "AIzaSyC0i_PUEFPoybnsVaCGlk_J-FronkQJMIA",
+  authDomain:    process.env.REACT_APP_FIREBASE_PROJECT_ID     ? `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com` : "poker-tournament-b845f.firebaseapp.com",
+  databaseURL:   process.env.REACT_APP_FIREBASE_DB_URL         || "https://poker-tournament-b845f-default-rtdb.firebaseio.com",
+  projectId:     process.env.REACT_APP_FIREBASE_PROJECT_ID     || "poker-tournament-b845f",
+  storageBucket: process.env.REACT_APP_FIREBASE_PROJECT_ID     ? `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebasestorage.app` : "poker-tournament-b845f.firebasestorage.app",
+  messagingSenderId: "758629753463",
+  appId:         process.env.REACT_APP_FIREBASE_APP_ID         || "1:758629753463:web:8485cd78a87b048a465c72"
 };
 
 const app = initializeApp(firebaseConfig);
